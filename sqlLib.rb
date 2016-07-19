7c53fd86e05e6be73982a0958106886c3989dab4
themaxta <themaxta@gmail.com>
2016-07-19 14:59:27 -0600

    basic account managing functionality finished

--- sqlLib.rb
+++ sqlLib.rb
@@ -1,7 +1,6 @@
 require 'sqlite3'
 $db = SQLite3::Database.new("dbfile")
 $db.results_as_hash = true
-puts $db
 
 
 
@@ -12,6 +11,7 @@ puts $db
 		$session_job = nil
 		$session_age = nil
 		$session_gender = nil
+		$session_access = nil
 	end
 
 	def set_global_session_vars(person_instance)
@@ -25,8 +25,123 @@ puts $db
 		$session_age      = person_instance['age']
 		$session_gender   = person_instance['gender']
 		$session_password = person_instance['password']
+		$session_access   = person_instance['access']
+	end
+
+	def set_access
+		person_instance = find_person
+		puts "\n\n\n\n=========================================="
+		puts "Currently Selected Account: "
+		puts "==========================================\n\n\n"
+
+		sleep(0.5)
+		puts "-  Username: #{person_instance['name']}. "
+		puts
+		sleep(0.5)
+		puts "-  Internal User Id: #{person_instance['id']}. "
+		puts
+		sleep(0.5)
+
+		puts "-  Account was created on 00/00/00."
+		puts
+		puts "\n\n"
+
+		sleep(0.5)
+
+		puts "  Give this account administration access? "
+		puts "     -------| Yes | or | No |-------       "
+		sleep(1.8)
+		puts
+		puts
+		response = gets.chomp
+		if response.downcase == 'yes'
+			person_instance['access'] = "Admin"
+			puts "\n\n\n\n\n\n"
+			sleep(1)
+			puts "  !!   Admin Privileges Have Been Granted   !!\n\n\n"
+			sleep(2)
+		else
+
+			sleep(1)
+			return
+		end
+
 
 	end
+
+
+
+
+
+
+
+
+
+
+
+	def scroll_display
+		10.times do 
+			puts "   -----         -----         -----         -----         -----         -----         -----\n"
+			sleep(0.12)
+		end #end loop
+		puts "\n\n"
+
+		sleep(0.12)
+
+		puts "                 Welcome    To    Account    Simulator    2016   ! ! !      "
+
+		sleep(0.12)
+
+		puts "\n"
+
+		sleep(0.12)
+
+		puts "                 Coded by: Max Mahlke "
+
+		sleep(0.12)
+
+		puts "\n\n"
+
+
+		5.times do 
+			puts "   -----         -----         -----         -----         -----         -----         -----\n"
+			sleep(0.12)
+		end #end loop
+		sleep(1)
+		15.times do
+			puts "   -----         -----         -----         -----         -----         -----         -----\n"
+			sleep(0.12)
+		end
+	end
+
+	def loading_display
+		puts "\n\n\n\n\n\n\n"
+		puts "--------------------------------------------------------------------------------"
+		puts "                   LOADING DATABASE CONFIGURATIONS..."
+		puts "--------------------------------------------------------------------------------"
+
+		puts "\n\n\n"
+		print "| "
+		25.times do
+			print "==="
+			sleep(0.1)
+		end
+
+		print " |"
+
+		puts "\n\n\n"
+
+
+	end
+
+
+
+
+
+
+
+
+
 
 	def login
 		#for username
@@ -76,7 +191,8 @@ puts $db
 				job varchar(50),
 				gender varchar(6),
 				age integer,
-				password varchar(32)
+				password varchar(32),
+				access varchar(15)
 
 				)
 		}
@@ -119,11 +235,12 @@ puts $db
 
 		#now, use these var's to create a person instance in sql db
 		$db.execute(
-			"INSERT INTO people (name, job, gender, age, password) VALUES (?,?,?,?,?)", name, job, gender, age, password)
+			"INSERT INTO people (name, job, gender, age, password, access) VALUES (?,?,?,?,?,?)", name, job, gender, age, password, "Basic")
 		
 	end
 
 	def find_person
+		puts "\n\n"
 		puts "Enter a name or ID of person to find:"
 		id = gets.chomp
 
@@ -136,10 +253,12 @@ puts $db
 
 		#output query results
 		puts %Q{	
-					Name: #{person['name'] }
-					Job: #{person['job'] }
+					Name:   #{person['name'] }
+					Job:    #{person['job'] }
 					Gender: #{person['gender'] }
-					Age: #{person['age'] }   }
+					Age:    #{person['age'] }
+					Access: #{person['access']}   }
+
 
 		return(person)
 	end
@@ -150,36 +269,50 @@ puts $db
 #on startup, reset all session variables
 init_global_session_vars
 
+scroll_display
+
 	loop do 
 		puts "\n\n"
 
 
 		if $session_id == nil 
 			
-
-			puts %q{	Please select an option: 
-
-				1. Create people table.
-				2. Add a person.
-				3. Look for a person.
-				4. Login.
+			puts "  =============================================================================="
+			puts %q{  Please select an option: 
+  ==============================================================================
 
 
-				9. Quit.
+			(1) --  Create people table.
+			(2) --  Add a person.
+			(3) --  Look for a person.
+			(4) --  Login.
+
+			(7) --  Set admin accounts.
+			(8) --  Drop people table.
+			(9) --  Quit.
 
 
 
 				}
 				case gets.chomp
 				when '1'
+					loading_display
 					create_table
 				when '2'
 					add_person
 				when '3'
 					person_instance = find_person
 				when '4'
+					loading_display
 					login
-
+				when '5'
+					#future use
+				when '6'
+					loading_display
+				when '7'
+					set_access
+				when '8'
+					delete_table
 				when '9'
 					disconnect_and_quit
 				else
@@ -189,21 +322,21 @@ init_global_session_vars
 		else
 
 			puts '-----------------------------------------------------------------'
-			puts "Current Session = #{$session_id}   ||   User: #{$session_name}   ||   Gender: #{$session_gender}"
+			puts "Current Access = #{$session_access}   ||   User: #{$session_name}   ||   Gender: #{$session_gender}"
 			puts '-----------------------------------------------------------------'
 			puts "\n\n"
 
 
 			puts %q{	Please select an option: 
 
-				1. Create people table.
-				2. Add a person.
-				3. Look for a person.
-				4. Logout.
+				(1) -- Create people table.
+				(2) -- Add a person.
+				(3) -- Look for a person.
+				(4) -- Logout.
 
 
-				8. Drop people table.
-				9. Quit.
+				(8) -- Drop people table.
+				(9) -- Quit.
 
 				}
 				case gets.chomp
@@ -228,7 +361,7 @@ init_global_session_vars
 
 
 			puts "\n\n"
-			puts "Press Enter to return to Menu."
+			puts "Press Enter to continue. "
 			puts "\n"
 			gets.chomp
 
