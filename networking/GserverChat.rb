1359ae0a688e598c69effc216d034c0e996abed9
themaxta <themaxta@gmail.com>
2016-08-19 18:26:55 -0600

    client can shutdown server

--- networking/GserverChat.rb
+++ networking/GserverChat.rb
@@ -5,9 +5,12 @@ require_relative 'wordplay'
 
 class HelloServer < GServer 
 	def serve(io)
+		puts "To exit this server, type 'shutdown' through a client..."
+		io.puts("To exit Server, Type 'shutdown' as a response....")
 		loop do
 			io.puts("Say something to me:")
 			line = io.gets
+			self.stop if line =~ /shutdown/
 			line_swap = WordPlay.switch_pronouns(line)
 			io.puts("You said '#{line.chomp}.'")
 			io.puts("The Server says, '#{line_swap}...'")
@@ -17,4 +20,9 @@ class HelloServer < GServer
 end
 server = HelloServer.new(1234, '127.0.0.1', 1)
 server.start
-server.join
+
+loop do
+	break if server.stopped?
+end
+
+puts "Server has been terminated"
