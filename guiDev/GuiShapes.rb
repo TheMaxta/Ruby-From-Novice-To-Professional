206edd7b34a57e668737935b134eb13007639b13
themaxta <themaxta@gmail.com>
2016-08-20 03:42:53 -0600

    gui shapes and text experimenting

---
+++ guiDev/GuiShapes.rb
@@ -0,0 +1,23 @@
+Shoes.app do 
+	fill black
+	stroke blue
+	oval :left => 10, :top => 10, :radius => 50
+	oval :left => 110, :top => 70, :radius => 20
+	fill blue
+	stroke green
+	oval :left => 160, :top => 80, :radius => 10
+
+	line 100, 150, 250, 200
+	line 200, 250, 150, 100
+
+	fill black
+	stroke red
+	rect 290,40,150,75
+	rect 290,150,250,100,5
+
+	fill yellow
+	stroke black
+	arrow 220,70,50
+
+
+end
---
+++ guiDev/GuiText.rb
@@ -0,0 +1,9 @@
+Shoes.app do 
+	stack do 
+		title "My Application!"
+		subtitle "It's a Great Application.."
+		para "Hello! This is a ", em("really"), " great app!"
+		para "How about some ", strong("bold"), " text??"
+
+	end #end stack
+end #end app
